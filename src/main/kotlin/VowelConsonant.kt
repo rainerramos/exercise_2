d16349e6c817fd354e668eaef98a996b8fdbf626
Rainer Ramos <rainermr89@hotmail.com>
2021-07-20 12:04:04 -0300

    Resolução exercício 2

--- src/main/kotlin/VowelConsonant.kt
+++ src/main/kotlin/VowelConsonant.kt
@@ -1,7 +1,19 @@
 fun countVowels(phrase: String): Int {
-    return 0
+    val VOWELS = "aeiou"
+    var totalVowels = 0
+
+    for(letter in phrase){
+        if(letter.lowercaseChar() in VOWELS) totalVowels++
+    }
+    return totalVowels
 }
 
 fun countConsonants(phrase: String): Int {
-    return 0
+    val CONSONANTS = "bcdfghjklmnpqrstvwxyz"
+    var totalConsonants = 0
+
+    for(letter in phrase){
+        if(letter.lowercaseChar() in CONSONANTS) totalConsonants++
+    }
+    return totalConsonants
 }
